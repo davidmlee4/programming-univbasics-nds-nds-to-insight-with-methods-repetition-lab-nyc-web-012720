d62af6fcb9686f7364c0ddcc63e0ec7a4503616f
Muhan Lee <davidmlee4@gmail.com>
2020-01-26 22:00:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -42,11 +42,13 @@ directors_earnings_hash = directors_totals(source)
 list = list_of_directors(source)
 pp source
 i = 0
+total = 0
+
   # Should use methods:
   # 1. directors_totals: returns a Hash of { dir_name => gross }
   # 2. list_of_directors: names provides an Array of directors names (use
   #
   # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
+  # returned by directors_totals, and add it to a running total. Think grand_total = 0
+  # When done return the total
 end
